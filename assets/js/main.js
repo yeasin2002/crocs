5497b4d5115d6895c6ef1d4a74d2f9f14709b8f9
yeasin2002 <mdkawsarislam2002@gmail.com>
2025-04-03 02:37:46 +0600

    feat: added AOS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,24 @@
-    
-    
-    $(document).ready(function () {
-
-     $(window).on('scroll',function() {    
-   var scroll = $(window).scrollTop();
-   if (scroll < 10) {
-    $(".header-middle").removeClass("scroll-header");
-   }else{
-    $(".header-middle").addClass("scroll-header");
-   }
+$(document).ready(function () {
+  $(window).on("scroll", function () {
+    var scroll = $(window).scrollTop();
+    if (scroll < 10) {
+      $(".header-middle").removeClass("scroll-header");
+    } else {
+      $(".header-middle").addClass("scroll-header");
+    }
   });
-  
-        
+
   //    click-action
-        $(".header__bar").on("click", function() {
-            $(".offcanva, .overlay").addClass("active");
-            return false;
-        });
+  $(".header__bar").on("click", function () {
+    $(".offcanva, .overlay").addClass("active");
+    return false;
+  });
 
-        $(".cross").on("click", function() {
-            $(".offcanva, .overlay").removeClass("active");
-        });
+  $(".cross").on("click", function () {
+    $(".offcanva, .overlay").removeClass("active");
+  });
 
-        $(".overlay").on("click", function() {
-            $(".offcanva, .overlay").removeClass("active");
-        });
-
-
-    }); //---document-ready-----
+  $(".overlay").on("click", function () {
+    $(".offcanva, .overlay").removeClass("active");
+  });
+}); //---document-ready-----
