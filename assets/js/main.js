e58473733bf2f5e1cce582f23f0fb793708c3746
yeasin2002 <mdkawsarislam2002@gmail.com>
2025-04-03 03:03:13 +0600

    feat: added v2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,30 @@
 $(document).ready(function () {
-  $(window).on("scroll", function () {
-    var scroll = $(window).scrollTop();
-    if (scroll < 10) {
-      $(".header-middle").removeClass("scroll-header");
-    } else {
-      $(".header-middle").addClass("scroll-header");
-    }
-  });
+   $(window).on("scroll", function () {
+      var scroll = $(window).scrollTop();
+      if (scroll < 10) {
+         $(".header-middle").removeClass("scroll-header");
+      } else {
+         $(".header-middle").addClass("scroll-header");
+      }
+   });
 
-  //    click-action
-  $(".header__bar").on("click", function () {
-    $(".offcanva, .overlay").addClass("active");
-    return false;
-  });
+   //    click-action
+   $(".header__bar").on("click", function () {
+      $(".offcanva, .overlay").addClass("active");
+      return false;
+   });
 
-  $(".cross").on("click", function () {
-    $(".offcanva, .overlay").removeClass("active");
-  });
+   $(".cross").on("click", function () {
+      $(".offcanva, .overlay").removeClass("active");
+   });
 
-  $(".overlay").on("click", function () {
-    $(".offcanva, .overlay").removeClass("active");
-  });
+   $(".overlay").on("click", function () {
+      $(".offcanva, .overlay").removeClass("active");
+   });
 }); //---document-ready-----
+
+
+// venobox active
+new VenoBox({
+   selector: ".play-video",
+});
